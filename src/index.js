dde226d251b5cf773835ae4f88c67ba147c13d30
MC Guenette <mc.c.guenette@gmail.com>
2024-06-14 09:33:28 -0500

    trying to use hashrouter instead

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    {/* <Router> */}
+    <HashRouter>
     <App />
-    </Router>
+    {/* </Router> */}
+    </HashRouter>
   </React.StrictMode>
 );

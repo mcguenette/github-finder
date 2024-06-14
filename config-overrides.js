a835b3443dc524eb54983b285589427bf68cce2c
MC Guenette <mc.c.guenette@gmail.com>
2024-06-14 09:12:14 -0500

    trying to resolve prod issues

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
+const webpack = require('webpack');
+
 module.exports = function override(config) {
     config.resolve.fallback = {
         ...config.resolve.fallback,
-        fs: false, // Since you no longer use `dotenv`
+        fs: false,
         os: require.resolve('os-browserify/browser'),
-        path: require.resolve('path-browserify')
+        path: require.resolve('path-browserify'),
+        process: require.resolve('process/browser')
     };
+
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ]);
+
     return config;
 };
